23260f75f1e350a5c8cf02f9fabb3ef11d241e29
liam <liam.wang@live.com>
2019-01-11 19:36:19 +0800

    添加 Web API 示例

---
+++ Sample.WebApi/Repositories/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using Anet.Data;
+using Sample.WebApi.Models.Dtos;
+using Sample.WebApi.Models.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Sample.WebApi.Repositories
+{
+    public class UserRepository : RepositoryBase<User>
+    {
+        public UserRepository(Database db) : base(db)
+        {
+        }
+
+        public Task<IEnumerable<UserResponseDto>> GetAllAsync()
+        {
+            var sql = "SELECT * FROM User;";
+            return Db.QueryAsync<UserResponseDto>(sql);
+        }
+
+        public Task<UserResponseDto> GetByIdAsync(long id)
+        {
+            var sql = Sql.Select("User", new { Id = id });
+            return Db.QueryFirstOrDefaultAsync<UserResponseDto>(sql);
+        }
+    }
+}
---
+++ Sample.WebApi/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Sample.WebApi.Repositories;
+using Sample.WebApi.Services;
+using System.Data.SqlClient;
+
+namespace Sample.WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDatabase<SqlConnection>(Configuration.GetConnectionString("DefaultConnection"));
+
+            services.AddAnetMvcCore().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddTransient<UserRepository>();
+            services.AddTransient<UserService>();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
