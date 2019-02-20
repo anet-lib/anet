5a444a9cc84d4fcb15f7f1b82aa781f077e8cfc8
Zae <zaevi@live.com>
2019-02-21 02:02:10 +0800

    重构调度器

--- Anet.Job/Scheduler.cs
+++ Anet.Job/Scheduler.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -8,94 +11,166 @@ namespace Anet.Job
 {
     public class Scheduler
     {
+
+        public static bool Enabled { get; private set; } = true;
+
         public static bool IsStopping { get; private set; }
 
-        private static volatile int _runningTaskCount;
+        private static Timer _timer = new Timer(s => OnTimerCallback(), null, -1, -1);
 
-        public static void IncreTaskCount()
+        private static List<Schedule> _scheduleList = new List<Schedule>();
+
+        private static Schedule _nextToRun = null;
+
+        private static HashSet<(Schedule, Task)> _running = new HashSet<(Schedule, Task)>();
+
+        private static void UpdateTimer()
         {
-            Interlocked.Increment(ref _runningTaskCount);
+            var nextToRun = _scheduleList.OrderBy(s => s.NextRun).FirstOrDefault();
+            if(nextToRun == null)
+            {
+                _timer.Change(-1, -1);
+            }
+            else
+            {
+                _nextToRun = nextToRun;
+                var interval = nextToRun.NextRun - DateTime.Now;
+                if(interval <= TimeSpan.Zero)
+                {
+                    OnTimerCallback();
+                }
+                else
+                {
+                    _timer.Change(interval, interval);
+                }
+            }
+                
         }
 
-        public static void DecreTaskCount()
+        private static void RunJob(Schedule schedule)
         {
-            Interlocked.Decrement(ref _runningTaskCount);
+            schedule.NextRun = schedule.NextRun + schedule.Interval;
+
+            lock (_running)
+            {
+                if (_running.Any(t => ReferenceEquals(t.Item1, schedule)))
+                    return;
+            }
+
+            (Schedule, Task) tuple = (null, null);
+
+            var task = new Task(() => 
+            {
+                var scope = App.ServiceProvider.CreateScope();
+                var job = scope.ServiceProvider.GetRequiredService(schedule.JobType) as IJob;
+                try
+                {
+                    job.ExecuteAsync().Wait();
+                }
+                catch (Exception ex)
+                {
+                    try
+                    {
+                        job.OnExceptionAsync(ex).Wait();
+                    }
+                    catch (Exception innerEx)
+                    {
+                        var logger = scope.ServiceProvider.GetService<ILogger<Scheduler>>();
+                        logger?.LogError(innerEx, "任务内部处理执行异常。");
+                    }
+                }
+                finally
+                {
+                    lock (_running)
+                    {
+                        _running.Remove(tuple);
+                    }
+                    scope.Dispose();
+                }
+            }, TaskCreationOptions.PreferFairness);
+
+            tuple = (schedule, task);
+
+            lock(_running)
+            {
+                _running.Add(tuple);
+            }
+
+            task.Start();
+
+        }
+
+        private static void OnTimerCallback()
+        {
+            Debug.WriteLine($"run {_nextToRun.JobType} {_nextToRun.NextRun}");
+
+            RunJob(_nextToRun);
+            UpdateTimer();
         }
 
         /// <summary>
         /// 开启新的任务调度
         /// </summary>
         /// <param name="seconds">任务间隔秒数</param>
-        public static Task StartNew<T>(int seconds) where T : IJob
+        public static void StartNew<T>(int seconds) where T : IJob
         {
-            return StartNew<T>(TimeSpan.FromSeconds(seconds));
+            StartNew<T>(TimeSpan.FromSeconds(seconds));
         }
 
         /// <summary>
         /// 开启新的任务调度
         /// </summary>
-        /// <param name="intervel">任务间隔</param>
-        public static Task StartNew<T>(TimeSpan intervel) where T : IJob
+        /// <param name="interval">任务间隔</param>
+        public static void StartNew<T>(TimeSpan interval) where T : IJob
         {
-            return Task.Factory.StartNew(() =>
+            var schedule = new Schedule()
             {
-                while (true)
-                {
-                    if (IsStopping) break;
-                    IncreTaskCount();
+                JobType = typeof(T),
+                Interval = interval,
+                NextRun = DateTime.Now + interval
+            };
 
-                    var scope = App.ServiceProvider.CreateScope();
-                    var job = scope.ServiceProvider.GetRequiredService<T>();
-                    try
-                    {
-                        job.ExecuteAsync().GetAwaiter().GetResult();
-                    }
-                    catch (Exception ex)
-                    {
-                        try
-                        {
-                            job.OnExceptionAsync(ex);
-                        }
-                        catch (Exception innerEx)
-                        {
-                            var logger = scope.ServiceProvider.GetService<ILogger<Scheduler>>();
-                            logger?.LogError(innerEx, "任务内部处理执行异常。");
-                        }
-                    }
-                    finally
-                    {
-                        DecreTaskCount();
-                        scope.Dispose();
-                    }
-                    Thread.Sleep(intervel);
-                }
-            }, TaskCreationOptions.LongRunning);
+            _scheduleList.Add(schedule);
+            UpdateTimer();
         }
 
+        
         /// <summary>
         /// 等待程序关闭
         /// </summary>
         public static void WaitForShutdown()
         {
-            void Shutdown()
-            {
-                Console.WriteLine("程序正在关闭，请稍后...");
-                StopAll();
-            }
-
-            AppDomain.CurrentDomain.ProcessExit += (sender, eventArgs) => Shutdown();
-            Console.CancelKeyPress += (s, e) => Shutdown();
+            AppDomain.CurrentDomain.ProcessExit += (sender, eventArgs) => StopAll();
+            Console.CancelKeyPress += (s, e) => StopAll();
             Thread.Sleep(Timeout.Infinite);
         }
 
         /// <summary>
         /// 停止所有任务
         /// </summary>
-        private static void StopAll()
+        public static void StopAll()
         {
-            if (IsStopping) return;
-            IsStopping = true;
-            while (_runningTaskCount > 0) ;
+            Enabled = false;
+            _timer.Change(-1, -1);
+        }
+
+        /// <summary>
+        /// 停止所有任务并等待
+        /// </summary>
+        public static void StopAllAndWait()
+        {
+            StopAll();
+
+            var tasks = new Task[0];
+
+            do
+            {
+                lock (_running)
+                {
+                    tasks = _running.Select(t => t.Item2).ToArray();
+                }
+                Task.WaitAll(tasks);
+            } while (tasks.Any());
         }
     }
 }
