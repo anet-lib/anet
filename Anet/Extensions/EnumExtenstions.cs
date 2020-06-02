fa2288aa5f8fa122c13dfeb7ee30d09cfdbe1c61
liam <liam.wang@live.com>
2020-06-02 11:08:41 +0800

    增加枚举扩展方法

--- Anet/Extensions/EnumExtenstions.cs
+++ Anet/Extensions/EnumExtenstions.cs
@@ -1,16 +1,33 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Reflection;
 
 namespace System
 {
     public static class EnumExtenstions
     {
+        public static T GetCustomAttribute<T>(this Enum value) where T : Attribute
+        {
+            var field = value.GetType().GetField(value.ToString());
+            return field.GetCustomAttribute<T>();
+        }
+
         public static string GetDisplayName(this Enum value)
         {
-            var type = value.GetType();
-            var name = Enum.GetName(type, value);
-            var attribute = type.GetField(name).GetCustomAttribute<DisplayAttribute>();
-            return attribute == null ? name : attribute.Name;
+            var attribute = value.GetCustomAttribute<DisplayAttribute>();
+            return attribute == null ? value.ToString() : attribute.Name;
+        }
+
+        public static string GetDisplayDescription(this Enum value)
+        {
+            var attribute = value.GetCustomAttribute<DisplayAttribute>();
+            return attribute == null ? value.ToString() : attribute.Description;
+        }
+
+        public static string GetDescription(this Enum value)
+        {
+            var attribute = value.GetCustomAttribute<DescriptionAttribute>();
+            return attribute == null ? value.ToString() : attribute.Description;
         }
     }
 }
