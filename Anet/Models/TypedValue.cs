2cdac7f54d6bb6f15c846b3d5e8484ad90cf9e6b
liam <liam.wang@live.com>
2020-06-05 16:07:08 +0800

    add TypedValue model

--- Anet/Models/TypedValue.cs
+++ Anet/Models/TypedValue.cs
@@ -2,6 +2,26 @@
 {
     public class TypedValue<T>
     {
+        public TypedValue()
+        {
+        }
+
+        public TypedValue(T value)
+        {
+            Value = value;
+        }
+
         public T Value { get; set; }
+    }
+
+    public class TypedValue : TypedValue<string>
+    {
+        public TypedValue() : base()
+        {
+        }
+
+        public TypedValue(string value) : base(value)
+        {
+        }
     }
 }
