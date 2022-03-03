a87c34a555ed5de5259fae2b8349c66f2dd3d2ac
Liam <liam.wang@live.com>
2022-03-03 15:22:36 +0800

    优化Jwt中间件注册

--- Anet.Web/AppBuilderExtensions.cs
+++ Anet.Web/AppBuilderExtensions.cs
@@ -10,9 +10,17 @@ public static class AppBuilderExtensions
         app.UseMiddleware<ExceptionMiddleware>(pathPrefixes?.ToHashSet());
     }
 
+    /// <summary>
+    /// Adds the to the <see cref="JwtProviderMiddleware"/> and <see cref="Authentication.AuthenticationMiddleware"/> to the
+    /// specified <see cref="IApplicationBuilder"/>.
+    /// </summary>
+    /// <param name="app">The <see cref="IApplicationBuilder"/> to add the middleware to.</param>
+    /// <param name="tokenPath">The api path used to get the token.</param>
+    /// <returns></returns>
     public static IApplicationBuilder UseAnetJwtAuthentication(this IApplicationBuilder app, string tokenPath)
     {
         app.UseMiddleware<JwtProviderMiddleware>(tokenPath);
+        app.UseAuthentication();
         return app;
     }
 }
