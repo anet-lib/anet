fd227260a8af1bfb5af3c4ca9a2d20b9a7a8f156
Liam <liam.wang@live.com>
2022-09-29 16:39:57 +0800

    v0.7.24

--- Anet/Extensions/LinqExtensions.cs
+++ Anet/Extensions/LinqExtensions.cs
@@ -2,19 +2,18 @@
 
 public static class LinqExtensions
 {
-    public static IEnumerable<TSource> DistinctBy<TSource, TKey>(
-        this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
-    {
-        HashSet<TKey> knownKeys = new();
-
-        foreach (TSource element in source)
-        {
-            if (knownKeys.Add(keySelector(element)))
-            {
-                yield return element;
-            }
-        }
-    }
+    //public static IEnumerable<TSource> DistinctBy<TSource, TKey>(
+    //    this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+    //{
+    //    HashSet<TKey> knownKeys = new();
+    //    foreach (TSource element in source)
+    //    {
+    //        if (knownKeys.Add(keySelector(element)))
+    //        {
+    //            yield return element;
+    //        }
+    //    }
+    //}
 
     public static IQueryable<T> Paged<T>(this IQueryable<T> query, int page, int size)
     {
