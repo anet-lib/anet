5f3404e561159b2ddf685d8e50b20050fb192803
Liam <liam.wang@live.com>
2023-06-18 17:20:16 +0800

    minimize json serialize  escaping

--- Anet/Utilities/Json.cs
+++ Anet/Utilities/Json.cs
@@ -1,24 +1,35 @@
-﻿using System.Text.Json;
+﻿using System.Text.Encodings.Web;
+using System.Text.Json;
 
 namespace Anet.Utilities;
 
 public class Json
 {
+    private static readonly JsonSerializerOptions _defaultOptions = new()
+    {
+        PropertyNameCaseInsensitive = true,
+
+        // ref: https://learn.microsoft.com/en-us/dotnet/standard/serialization/system-text-json/character-encoding
+        Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+    };
+
     private static readonly JsonSerializerOptions _camelCaseOptions = new()
     {
         PropertyNameCaseInsensitive = true,
         PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
     };
 
     private static readonly JsonSerializerOptions _snakeCaseOptions = new()
     {
         PropertyNameCaseInsensitive = true,
         PropertyNamingPolicy = new SnakeCaseNamingPolicy(),
+        Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
     };
 
     public static string Serialize(object value, JsonSerializerOptions options = null)
     {
-        return JsonSerializer.Serialize(value, options);
+        return JsonSerializer.Serialize(value, options ?? _defaultOptions);
     }
 
     public static T Deserialize<T>(string json, JsonSerializerOptions options = null)
