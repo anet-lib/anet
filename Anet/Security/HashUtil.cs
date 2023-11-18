fada5a56e1e6d112a495e82ebe226a84a5b9aa91
Liam <liam.wang@live.com>
2023-11-18 22:23:16 +0800

    Optimize encryption methods

---
+++ Anet/Security/HashUtil.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+namespace Anet.Security;
+
+public static class HashUtil
+{
+    public static string BytesToString(byte[] bytes)
+    {
+        var sb = new StringBuilder();
+        foreach (byte b in bytes)
+        {
+            sb.Append(b.ToString("x2"));
+        }
+        return sb.ToString();
+    }
+}
--- Anet/Security/SHA1.cs
+++ Anet/Security/SHA1.cs
@@ -1,10 +1,10 @@
 ﻿using System.Text;
 
-using Hasher = System.Security.Cryptography.MD5;
+using Hasher = System.Security.Cryptography.SHA1;
 
 namespace Anet.Security;
 
-public static class MD5
+public static class SHA1
 {
     public static string Hash(string input)
     {
@@ -20,8 +20,8 @@ public static class MD5
 
     public static string Hash(Stream stream)
     {
-        using var md5 = Hasher.Create();
-        byte[] result = md5.ComputeHash(stream);
+        using var sha1 = Hasher.Create();
+        byte[] result = sha1.ComputeHash(stream);
         return HashUtil.BytesToString(result);
     }
 }
